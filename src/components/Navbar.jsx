8ff4687db15520722ccf55a27d64cc9c6a9926a8
rahmaninsani <rahmaninsani54@gmail.com>
2021-10-20 11:53:59 +0700

    setup the router and each link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Search } from "./Search";
+
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
@@ -12,6 +14,7 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
           {darkTheme ? "Light 💡" : "Dark 🌙"}
         </button>
       </div>
+      <Search />
     </div>
   );
 };
