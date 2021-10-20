d83451cb7f6b383fb2dbbcbed13838cd9285f7fb
rahmaninsani <rahmaninsani54@gmail.com>
2021-10-20 18:49:58 +0700

    update NavBar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import { Links } from "./Links";
+
 export const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      Search
+      <Links />
+    </div>
+  );
 };
